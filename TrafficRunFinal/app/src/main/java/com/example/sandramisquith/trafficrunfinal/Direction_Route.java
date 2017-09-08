b96620523200f7b84a44696f81855eb2de1d051f
ashcolaco <31735388+ashcolaco@users.noreply.github.com>
2017-09-08 21:04:36 +0530

    1st commit

---
+++ TrafficRunFinal/app/src/main/java/com/example/sandramisquith/trafficrunfinal/Direction_Route.java
@@ -0,0 +1,68 @@
+//Code to find location of vehicle and plot it on the maps
+package com.example.sandramisquith.trafficrunfinal;
+
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+
+public abstract class Direction_Route extends FragmentActivity implements OnMapReadyCallback {
+
+    public LocationManager mlocManager;
+    public LocationListener mlocListener;
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_direction__route);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    public void gpsEnable ()
+    {
+        mlocManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+
+        mlocListener = new MyLocationListener();
+
+        mlocManager.requestLocationUpdates( LocationManager.GPS_PROVIDER, 0, 0, mlocListener);
+    }
+
+
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap, Double lati, Double longi) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng current = new LatLng(lati, longi);
+        mMap.addMarker(new MarkerOptions().position(current).title("Marker in Current Location"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(current));
+    }
+}
+
